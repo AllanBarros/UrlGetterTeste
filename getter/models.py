3f0d6c2409066011a85dbb1e0f51a529b249ce08
unknown <allanbmalves@gmail.com>
2020-10-01 19:51:27 -0300

    Setup banco de dados e configurações locais.

--- getter/models.py
+++ getter/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class url_inicial(models.Model):
+    id_url_inicial = models.AutoField(primary_key=True)
+    link = models.CharField(blank=True, null=True, max_length=255)
+
+class urls_relacionadas(models.Model):
+    id_urls_relacionadas = models.AutoField(primary_key=True)
+    id_url_inicial = models.ForeignKey('url_inicial', on_delete=models.CASCADE)
+    link = models.CharField(blank=True, null=True, max_length=255)
--- urlGetter/settings.py
+++ urlGetter/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from . import localConfig
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'getter',
 ]
 
 MIDDLEWARE = [
@@ -73,12 +74,8 @@ WSGI_APPLICATION = 'urlGetter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#Apontando para arquivo com configurações para rodar em ambiente local
+DATABASES = localConfig.DATABASES
 
 
 # Password validation
